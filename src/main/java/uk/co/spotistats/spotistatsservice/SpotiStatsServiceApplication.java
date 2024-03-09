b4971042615e612145a6f29a0dc9250bee820967
daniel.brown <daniel.brown@autotrader.co.uk>
2024-03-09 01:15:55 +0000

    git init

---
+++ src/main/java/uk/co/spotistats/spotistatsservice/SpotiStatsServiceApplication.java
@@ -0,0 +1,13 @@
+package uk.co.spotistats.spotistatsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotiStatsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotiStatsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uk/co/spotistats/spotistatsservice/SpotiStatsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.spotistats.spotistatsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotiStatsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
