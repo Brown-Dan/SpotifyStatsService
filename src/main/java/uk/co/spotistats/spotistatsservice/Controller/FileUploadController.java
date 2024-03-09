c4737398b317743405430cf5111ef3b0d85efe6a
daniel.brown <daniel.brown@autotrader.co.uk>
2024-03-09 05:14:21 +0000

    formatting

--- src/main/java/uk/co/spotistats/spotistatsservice/Controller/FileUploadController.java
+++ src/main/java/uk/co/spotistats/spotistatsservice/Controller/FileUploadController.java
@@ -3,7 +3,10 @@ package uk.co.spotistats.spotistatsservice.Controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import uk.co.spotistats.spotistatsservice.Controller.Mapper.MultipartFileToStreamingDataMapper;
 import uk.co.spotistats.spotistatsservice.Controller.Model.ApiResult;
@@ -12,9 +15,6 @@ import uk.co.spotistats.spotistatsservice.Domain.Response.Result;
 import uk.co.spotistats.spotistatsservice.Domain.Response.StreamingDataUploadResponse;
 import uk.co.spotistats.spotistatsservice.Domain.StreamingData;
 import uk.co.spotistats.spotistatsservice.Service.FileUploadService;
-
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/file")
@@ -30,14 +30,12 @@ public class FileUploadController {
 
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ApiResult<StreamingDataUploadResponse, Error>> upload(@RequestPart MultipartFile file) {
-        Result<StreamingData, Error> streamingDataResult = multipartFileToStreamingDataMapper.map(file);
+        Result<StreamingData, Error> fileMapResult = multipartFileToStreamingDataMapper.map(file);
 
-        if (streamingDataResult.isFailure()) {
-            return badRequest(streamingDataResult.getError());
+        if (fileMapResult.isFailure()) {
+            return badRequest(fileMapResult.getError());
         }
-
-        Result<StreamingDataUploadResponse, Error> streamingDataUploadResponseResult =
-                fileUploadService.uploadFile(streamingDataResult.getValue());
+        Result<StreamingDataUploadResponse, Error> streamingDataUploadResponseResult = fileUploadService.uploadFile(fileMapResult.getValue());
 
         return switch (streamingDataUploadResponseResult) {
             case Result.Success(StreamingDataUploadResponse streamingDataUploadResponse) ->
@@ -45,7 +43,6 @@ public class FileUploadController {
             case Result.Failure(Error error) -> badRequest(error);
         };
     }
-
 
     private <T> ResponseEntity<ApiResult<T, Error>> ok(T body) {
         return ResponseEntity.ok(ApiResult.success(body));
