c903cb3e9c120199146e76fb6d5b5dd5db8c6a9b
daniel.brown <daniel.brown@autotrader.co.uk>
2024-03-13 12:10:22 +0000

    test

--- src/main/java/uk/co/spotistats/spotistatsservice/Service/SpotifyAuthService.java
+++ src/main/java/uk/co/spotistats/spotistatsservice/Service/SpotifyAuthService.java
@@ -58,14 +58,13 @@ public class SpotifyAuthService {
     }
 
     public void authorize(String username) {
-        Response<JSONObject> response = traverson.from(SPOTIFY_AUTHORIZE_URL)
-                .withQueryParam("client_id", System.getenv("SPOTIFY_CLIENT_ID"))
-                .withQueryParam("response-type", "code")
+//        traverson.from(SPOTIFY_AUTHORIZE_URL)
+//                .withQueryParam("client_id", System.getenv("SPOTIFY_CLIENT_ID"))
+//                .withQueryParam("response-type", "code")
 //                .withQueryParam("redirect-uri", "https://spotifystats.co.uk/spotify/authenticate/callback")
 //                .withQueryParam("state", username)
 //                .withQueryParam("scope", "playlist-read-private user-follow-read user-top-read user-read-recently-played user-library-read")
-                .get();
-        System.out.println(response.getResource().toJSONString());
+//                .get();
     }
 
     public Result<SpotifyAuthData, Errors> exchangeAccessToken(String username, String accessToken) {
