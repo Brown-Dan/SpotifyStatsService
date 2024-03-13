ee51ff2ec89d90a17adf5b3e2f9e8ea4fb4fead1
daniel.brown <daniel.brown@autotrader.co.uk>
2024-03-13 19:35:24 +0000

    new schema

--- src/main/resources/db/migration/V005__remove_last_update_from_stream_data.sql
+++ src/main/resources/db/migration/V005__remove_last_update_from_stream_data.sql
@@ -1,2 +1,2 @@
-ALTER TABLE "StreamingData".streaming_data DROP COLUMN last_updated;
+ALTER TABLE "StreamingData".stream_data DROP COLUMN last_updated;
 ALTER TABLE "StreamingData".streaming_data ADD COLUMN last_updated timestamp
